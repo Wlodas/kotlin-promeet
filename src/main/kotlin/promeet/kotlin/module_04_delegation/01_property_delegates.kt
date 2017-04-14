ef7a6b2663e328fe1778963e35f2f301ace7a4ba
MadMax <madmax1028@gmail.com>
2017-04-14 19:39:20 +0200

    added property delegate example with PropertyChangeSupport

--- src/main/kotlin/promeet/kotlin/module_04_delegation/01_property_delegates.kt
+++ src/main/kotlin/promeet/kotlin/module_04_delegation/01_property_delegates.kt
@@ -1,6 +1,10 @@
 package promeet.kotlin.module_04_delegation
 
+import java.beans.PropertyChangeSupport
 import kotlin.properties.Delegates
+import kotlin.properties.ObservableProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 private class DelegatingDTO(map: Map<String, Any?>) {
 	val mapVal by map
@@ -24,6 +28,20 @@ private class DelegatingDTO(map: Map<String, Any?>) {
 	}
 }
 
+private fun <T> PropertyChangeSupport.createProperty(initialValue: T): ReadWriteProperty<Any?, T> = object : ObservableProperty<T>(initialValue) {
+	override fun afterChange(property: KProperty<*>, oldValue: T, newValue: T) {
+		this@createProperty.firePropertyChange(property.name, oldValue, newValue)
+	}
+}
+
+private class ActiveModelDTO {
+	val propertyChangeSupport = PropertyChangeSupport(this)
+	
+	var property1: String by propertyChangeSupport.createProperty("initial String")
+	
+	var property2: Int? by propertyChangeSupport.createProperty(null)
+}
+
 fun main(args: Array<String>) {
 	val dto = DelegatingDTO(mapOf("mapVal" to "value from the map"))
 	
@@ -41,4 +59,11 @@ fun main(args: Array<String>) {
 	println(dto.evenNumber)
 	dto.evenNumber = 3
 	println(dto.evenNumber)
+	
+	val activeModelDTO = ActiveModelDTO()
+	activeModelDTO.propertyChangeSupport.addPropertyChangeListener { event ->
+		println("Property '${event.propertyName}' changed value from '${event.oldValue}' to '${event.newValue}'.")
+	}
+	activeModelDTO.property1 = "new string"
+	activeModelDTO.property2 = 1
 }
