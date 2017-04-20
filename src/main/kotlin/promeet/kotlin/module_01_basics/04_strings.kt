96737101b3f4d6d2f1216f0508cb975c0be3a72a
MadMax <madmax1028@gmail.com>
2017-04-20 11:50:38 +0200

    added example with string margins

--- src/main/kotlin/promeet/kotlin/module_01_basics/04_strings.kt
+++ src/main/kotlin/promeet/kotlin/module_01_basics/04_strings.kt
@@ -4,14 +4,22 @@ fun main(args: Array<String>) {
 	val stringValue = "abcd"
 	
 	// string template
-	println("String '$stringValue' has length ${stringValue.length}")
+	println("String '$stringValue' has length ${stringValue.length}") // evaluated eagerly
+	// if you wanted to get Groovy' GString-like behaviour in Kotlin you could use lazy delegate
 	
 	// multiline string
-	val queryString = """
+	println()
+	println("""
 		SELECT * FROM Company c
 		JOIN FETCH c.employees
 		WHERE c.id = :id
-	"""
+	""".trimIndent())
+	
+	// alternative way
 	println()
-	println(queryString.trimIndent())
+	println("""
+		|SELECT * FROM Company c
+		|JOIN FETCH c.employees
+		|WHERE c.id = :id
+	""".trimMargin())
 }
